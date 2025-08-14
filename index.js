5a7e73f9bbc121b8f0afbc44b62dd0ee86689c23
vconti1 <vincentconti393@gmail.com>
2025-08-14 12:35:56 -0400

    format new chapter release notification box

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const { Client, GatewayIntentBits, Events, EmbedBuilder } = require('discord.js');
-const { token, mangadex_id } = require('./config.json');
+const { token, mangadex_id, lang_en, most_recent_chapter } = require('./config.json');
 const { get } = require('http');
 
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
@@ -12,15 +12,14 @@ client.login(token);
 client.on('messageCreate', async (message) => {
   if (message.content === '!box'){
   try {
-  const title = await getManga();
+  const data = await getManga();
 
    const embed = new EmbedBuilder()
    .setColor(0x0099FF)
-   .setTitle(title ?? 'This should return as fallback')
-   .setDescription('First ever chapter!')
+   .setTitle(`NEW CHAPTER RELEASE` ?? `Something went wrong`)
+   .setDescription(`Chapter ${data.attributes.chapter} | ${data.attributes.title}` ?? `Could not get chapter info`)
    .addFields(
-    { name: 'Field 1', value: 'STUFF' },
-    { name: 'Field 2', value: 'Another value here'}
+    { name: 'Read it now!', value: data.attributes.externalUrl }
    )
    .setTimestamp()
    .setFooter({ text: 'Box Bot', iconURL: 'https://i.imgur.com/AfFp7pu.png'})
@@ -34,13 +33,12 @@ client.on('messageCreate', async (message) => {
 })
 
 async function getManga(){
-  const mangaId = mangadex_id;
-  const url = new URL(`https://api.mangadex.org/manga/${mangaId}/feed?translatedLanguage[]=en`)
+  //const mangaId = mangadex_id;
+  const url = new URL(`https://api.mangadex.org/manga/${mangadex_id}/feed?${lang_en}&limit=1&${most_recent_chapter}`)
 
   const res =  await fetch(url);
   const json = await res.json();
   
-
-  return json.data[0].attributes.title;
+  return json.data[0]; 
 }
 
