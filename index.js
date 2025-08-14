dd5a8244e96409a02c6574c21ed21d819ac53403
vconti1 <vincentconti393@gmail.com>
2025-08-14 09:43:03 -0400

    send working message box

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-require('dotenv').config();
 
 const fs = require('fs');
 const path = require('path');
 
-const { Client, GatewayIntentBits, Events } = require('discord.js');
+const { Client, GatewayIntentBits, Events, EmbedBuilder } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
+client.login(token);
+
+client.on('messageCreate', message => {
+  if (message.content === '!box'){
+   const embed = new EmbedBuilder()
+   .setColor(0x0099FF)
+   .setTitle('This is a box message')
+   .setDescription('This is a cool ass despriction')
+   .addFields(
+    { name: 'Field 1', value: 'Some random ass value' },
+    { name: 'Field 2', value: 'Another value here', inline: true }
+   )
+   .setTimestamp()
+   .setFooter({ text: 'Box Bot', iconURL: 'https://i.imgur.com/AfFp7pu.png'})
+   
+   message.channel.send({ embeds: [embed]});
+  }
+  
+})
+
